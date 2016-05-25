8fe229f8c2435e3e1d45e8c0d9a64e7448159d76
Tom Oerlemans <tomoerlemans@hotmail.com>
2016-05-25 19:57:03 +0200

    in settings you can now adjust the time

--- app/src/main/java/com/example/tom/moble/MainActivity.java
+++ app/src/main/java/com/example/tom/moble/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.tom.moble;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -7,7 +9,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
 import org.w3c.dom.Text;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     TextView topText;
@@ -15,12 +22,15 @@ public class MainActivity extends AppCompatActivity {
     Button previousButton;
     int page = 0;
 
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.startscreen1);
         topText = (TextView) findViewById(R.id.topText);
         bottomText = (TextView) findViewById(R.id.bottomText);
         previousButton = (Button) findViewById(R.id.previousButton);
+
     }
 
     public void nextButtonClick(View view) {
@@ -50,6 +60,52 @@ public class MainActivity extends AppCompatActivity {
     public void settingsButtonClick(View view){
         setContentView(R.layout.settings);
     }
+
+    public void doneButtonClick(View view){
+        setContentView(R.layout.menu);
+    }
+
+    public void startTimeButtonClick(View view){
+        final TextView startTimeTextView = (TextView) findViewById(R.id.startTimeTextView);
+
+        TimePickerDialog tpd = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay,
+                                          int minute) {
+                        if (hourOfDay < 12){
+                            startTimeTextView.setText(String.format("%02d:%02d", hourOfDay, minute) + " AM");
+                        }else{
+                            startTimeTextView.setText(String.format("%02d:%02d", hourOfDay, minute)+ " PM");
+                        }
+
+                    }
+                }, 9, 0, false);
+        tpd.show();
+    }
+
+
+
+    public void endTimeButtonClick(View view){
+        final TextView endTimeTextView = (TextView) findViewById(R.id.endTimeTextView);
+
+        TimePickerDialog tpd = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay,
+                                          int minute) {
+                        if (hourOfDay < 12){
+                            endTimeTextView.setText(String.format("%02d:%02d", hourOfDay, minute) + " AM");
+                        }else{
+                            endTimeTextView.setText(String.format("%02d:%02d", hourOfDay, minute)+ " PM");
+                        }
+                    }
+                }, 21, 0, false);
+        tpd.show();
+    }
+
 
 
 }
