b7c640b8b8d0b23646da10951c9c1e674480368e
MRECoelho <mrecoelho@gmail.com>
2016-05-25 16:30:27 +0200

    miguels test comment commit

--- app/src/main/java/com/example/tom/moble/MainActivity.java
+++ app/src/main/java/com/example/tom/moble/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.tom.moble;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.w3c.dom.Text;
 
@@ -13,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
     TextView bottomText;
     Button previousButton;
     int page = 0;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +31,13 @@ public class MainActivity extends AppCompatActivity {
         topText = (TextView) findViewById(R.id.topText);
         bottomText = (TextView) findViewById(R.id.bottomText);
         previousButton = (Button) findViewById(R.id.previousButton);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public void nextButtonClick(View view){
-        if(page == 0) {
+    public void nextButtonClick(View view) {
+        if (page == 0) {
             topText.setText(getResources().getString(R.string.explanation_text1));
             bottomText.setText(getResources().getString(R.string.explanation_text2));
             previousButton.setVisibility(View.VISIBLE);
@@ -33,8 +46,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void previousButtonClick(View view){
-        if(page == 1) {
+    public void previousButtonClick(View view) {
+        if (page == 1) {
             topText.setText(getResources().getString(R.string.welcome_text1));
             bottomText.setText(getResources().getString(R.string.welcome_text2));
             previousButton.setVisibility(View.INVISIBLE);
@@ -43,4 +56,44 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.tom.moble/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.tom.moble/http/host/path")
+        );
+        // dit is mijn comment
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
