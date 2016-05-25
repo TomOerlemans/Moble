f2fe54f4389aed80de4d146ce72ad6b5720e2266
Tom Oerlemans <tomoerlemans@hotmail.com>
2016-05-25 16:30:32 +0200

    Added some fixes to the startmenu

--- app/src/main/java/com/example/tom/moble/MainActivity.java
+++ app/src/main/java/com/example/tom/moble/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
@@ -44,3 +43,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
+
+
+//dit is een test comment
